2d9005bf23c99ce1f188ca46395d30e0e004083d
Harrison Glenn <hglenn2k@gmail.com>
2025-04-20 18:12:18 -0700

    rewrite RouteGuard.js for new authcontext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
-import Account from "./pages/Account";
 import BecomeMember from "./pages/BecomeMember";
-import Calendar from "./pages/Calendar";
 import ContactUs from "./pages/ContactUs";
 import Faq from "./pages/Faq";
 import Forum from "./pages/Forum";
 import Home from "./pages/Home";
 import Map from "./pages/Map";
 import Resources from "./pages/Resources";
-import Search from "./pages/Search";
 import ContactList from "./pages/ContactList";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
@@ -18,19 +15,16 @@ import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import ThankYou from "./pages/ThankYou";
 import SubmitResource from "./pages/SubmitResource";
 import ThankYouContactUs from "./pages/ThankYouContactUs";
-import React from "react";
-import { useState } from "react";
-import Newsletter from "./pages/Newsletter";
+import React, { useState } from "react";
 import { useAuth } from "./contexts/AuthContext";
 import MembershipForm from "./pages/MembershipForm";
 import AdminPanel from "./pages/AdminPanel";
 import RenewMembership from "./pages/RenewMembership";
 import MembershipAcceptanceDialog from "./components/Dialogs/MembershipAcceptance";
 import { DialogProvider } from "./utils/DialogProvider";
-import { IsMemberGuard } from './utils/RouteGuards';
+import { RouteGuard } from "./utils/RouteGuard"; // Kept original name
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -42,31 +36,90 @@ function App() {
           <BrowserRouter>
             <Navbar open={open} setOpen={setOpen} />
             <Routes>
+              {/* Public */}
               <Route path="/" element={<Home />} />
+              <Route path="*" element={<Home />} />
               <Route path="/about" element={<About />} />
-              <Route path="/account" element={<Account />} />
-              <Route path="/become-a-member" element={<BecomeMember />} />
-              <Route path="/calendar" element={<IsMemberGuard user={user}><Calendar /></IsMemberGuard>} />
-              <Route path="/contact-list" element={<IsMemberGuard user={user}><ContactList /></IsMemberGuard>} />
               <Route path="/contact-us" element={<ContactUs />} />
               <Route path="/faq" element={<Faq />} />
-              <Route path="/forum" element={<IsMemberGuard user={user} uri="/forum"><Forum /></IsMemberGuard>} />
               <Route path="/map" element={<Map />} />
               <Route path="/resources" element={<Resources />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/newsletter" element={<Newsletter />} />
               <Route path="/privacy-policy" element={<PrivacyPolicy />} />
               <Route path="/terms-of-service" element={<TermsOfService />} />
               <Route path="/accessibility" element={<Accessibility />} />
               <Route path="/login" element={<Login />} />
               <Route path="/sign-up" element={<SignUp />} />
-              <Route path="submit-a-resource" element={<SubmitResource />} />
-              <Route path="/thank-you" element={<ThankYou />} />
               <Route path="/thank-you-contact-us" element={<ThankYouContactUs />} />
-              <Route path="/membership-form" element={<MembershipForm />} />
-              <Route path="/renew-membership" element={<RenewMembership />} />
-              <Route path="/admin-panel" element={<AdminPanel />} />
-              <Route path="*" element={<Home />} />
+
+              {/* Signed in, email verified (todo: split these up after adding email validation page */}
+              <Route path="/become-a-member" element={
+                <RouteGuard user={user} requires="isAuthenticated" uri="/login">
+                  <RouteGuard user={user} requires="isEmailVerified" uri="/">
+                    <BecomeMember />
+                  </RouteGuard>
+                </RouteGuard>
+              } />
+
+              <Route path="/membership-form" element={
+                <RouteGuard user={user} requires="isAuthenticated" uri="/login">
+                  <RouteGuard user={user} requires="isEmailVerified" uri="/">
+                    <MembershipForm />
+                  </RouteGuard>
+                </RouteGuard>
+              } />
+
+              <Route path="/renew-membership" element={
+                <RouteGuard user={user} requires="isAuthenticated" uri="/login">
+                  <RouteGuard user={user} requires="isEmailVerified" uri="/">
+                    <RenewMembership />
+                  </RouteGuard>
+                </RouteGuard>
+              } />
+
+              {/* Is member */}
+              <Route path="/contact-list" element={
+                <RouteGuard user={user} requires="isAuthenticated" uri="/login">
+                  <RouteGuard user={user} requires="isEmailVerified" uri="/">
+                    <RouteGuard user={user} requires="isMember" uri="/become-a-member">
+                      <ContactList />
+                    </RouteGuard>
+                  </RouteGuard>
+                </RouteGuard>
+              } />
+
+              <Route path="/forum" element={
+                <RouteGuard user={user} requires="isAuthenticated" uri="/login">
+                  <RouteGuard user={user} requires="isEmailVerified" uri="/">
+                    <RouteGuard user={user} requires="isMember" uri="/become-a-member">
+                      <Forum />
+                    </RouteGuard>
+                  </RouteGuard>
+                </RouteGuard>
+              } />
+
+              <Route path="/submit-a-resource" element={
+                <RouteGuard user={user} requires="isAuthenticated" uri="/login">
+                  <RouteGuard user={user} requires="isEmailVerified" uri="/">
+                    <RouteGuard user={user} requires="isMember" uri="/become-a-member">
+                      <SubmitResource />
+                    </RouteGuard>
+                  </RouteGuard>
+                </RouteGuard>
+              } />
+
+              {/* Admin Routes */}
+              <Route path="/admin-panel" element={
+                <RouteGuard user={user} requires="isAuthenticated" uri="/login">
+                  <RouteGuard user={user} requires="isEmailVerified" uri="/">
+                    <RouteGuard user={user} requires="isMember" uri="/become-a-member">
+                      <RouteGuard user={user} requires="isAdmin" uri="/login">
+                        <AdminPanel />
+                      </RouteGuard>
+                    </RouteGuard>
+                  </RouteGuard>
+                </RouteGuard>
+              } />
+
             </Routes>
             <Footer />
           </BrowserRouter>
